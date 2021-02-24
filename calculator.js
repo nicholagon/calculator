bc3aaec91946e7ad33df7f782bd838e46abc5a55
_weirdatlast <nicholagon@gmail.com>
2021-02-25 02:38:54 +0800

    Add initial calculator UI and operation functions

---
+++ calculator.js
@@ -0,0 +1,36 @@
+function operate(inputs) {
+    const [op, a, b] = inputs; 
+    const result;
+
+    switch(op) {
+        case '+':
+            result = add(a, b);
+            break;
+        case '-':
+            result = subtract(a, b);
+            break;
+        case '*':
+            result = multiply(a, b);
+            break;
+        case '/':
+            result = divide(a, b);
+            break;
+    }
+    return result
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
